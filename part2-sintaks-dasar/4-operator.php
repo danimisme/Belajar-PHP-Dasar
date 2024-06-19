9526534f4b36f37217dd0dae68e3c15d46ad27ec
danimisme <msubhanr53@gmail.com>
2024-06-19 22:13:09 +0700

    operator aritmatika

---
+++ part2-sintaks-dasar/4-operator.php
@@ -0,0 +1,34 @@
+<?php 
+//Operator
+//aritmatika + - * / %
+$x = 10;
+$y = 20;
+echo "x = $x , y = $y <br>";
+
+//penjumlahan
+echo " <br> penjumlahan x + y <br>";
+echo $x + $y;
+
+//pengurangan
+echo "<br> pengurangan x - y <br>";
+echo $x - $y;
+
+//perkalian
+echo " <br> perkalian x * y <br>";
+echo $x * $y;
+
+//pembagian
+echo "<br> pembagian x / y <br>";
+echo $x / $y;
+
+//modulus
+echo "<br> modulus x % y <br>";
+echo $x % $y;
+
+//pangkat
+echo "<br> pangkat x ** y <br>";
+echo $x ** $y;
+
+
+
+?>
