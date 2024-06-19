89d727a7789cb03ea425cf9f278f00b233dac298
danimisme <msubhanr53@gmail.com>
2024-06-19 21:56:30 +0700

    menulis tag htm dalam php , tidak disarankan !

---
+++ part2-sintaks-dasar/HTMLinPHP.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menulis tag html dalam php</title>
+</head>
+<body>
+    <?php echo"<h1>Halo , Selamat Datang Dhani</h1>"; ?>
+    <?php echo"<p>Cara ini tidak disarankan!</p>"; ?>
+</body>
+</html>
