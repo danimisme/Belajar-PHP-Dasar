ac6ee3e424483b87e2753d06b99e7918dfa0dec2
danimisme <msubhanr53@gmail.com>
2024-06-19 23:33:21 +0700

    pengulangan do while

--- part3-control-flow/pengulangan.php
+++ part3-control-flow/pengulangan.php
@@ -1,15 +1,22 @@
 <?php
 //Pengulangan for
 for( $i=0; $i < 5; $i++ ) {
-    echo "Hello World <br>";
+    echo "Hello World $i <br>";
 }
 
 
 //Pengulangan While
 $i = 0;
 while($i < 5){
-    echo "Pengulangan While <br>";
+    echo "Pengulangan While $i <br>";
     $i++;
 }
+
+$j = 0;
+//Pengulangan Do While
+do{
+    echo "Perulangan Do While $j <br>";
+    $j++;
+} while($j < 5);
 
 ?>
