3e1cbd61a41f1563dcb1618c12d658b8ba81cf02
danimisme <msubhanr53@gmail.com>
2024-06-19 22:23:22 +0700

    operator assignment

---
+++ part2-sintaks-dasar/6-operator-assignment.php
@@ -0,0 +1,17 @@
+<?php
+//Assignment
+// = , += , *= , %= , .=
+$x = 1;
+$x += 2;
+echo $x; // 3
+$x -= 2;
+echo "<br>";
+echo $x; // 1
+
+echo "<br>";
+$nama = "Muhammad";
+$nama .= " ";
+$nama .= "Subhan";
+echo $nama; // Muhammad Subhan
+
+?>
