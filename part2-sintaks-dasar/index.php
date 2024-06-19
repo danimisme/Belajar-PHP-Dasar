241b1ef306faf71b39d523b4c8951a4ff422001b
danimisme <msubhanr53@gmail.com>
2024-06-19 21:43:06 +0700

    Standar Output

---
+++ part2-sintaks-dasar/index.php
@@ -0,0 +1,20 @@
+<?php
+// Part 2 - PHP dasar
+//ini adalah komentar
+
+/*
+ini juga komentar
+komentar lebih dari satu baris
+*/
+
+// Standar Output
+// echo, print
+echo "Muhammad Subhan Ramdhani";
+print "PHP dasar - Part 2";
+// print_r
+print_r("Cetak menggunakan Print_R");
+// var_dump (memberikan informasi tipe data dan ukuran, biasa digunakan untuk debagging)
+var_dump("Muhammad Subhan Ramdhani")
+
+
+?>
