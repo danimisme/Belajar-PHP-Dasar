53e1703a10d3d791c1b0e5f2786f513cf0de1c05
danimisme <msubhanr53@gmail.com>
2024-06-19 22:26:49 +0700

    operator perbandingan

---
+++ part2-sintaks-dasar/7-operator-perbandingan.php
@@ -0,0 +1,21 @@
+<?php
+// Perbandingan
+// <, >, <=, >=, ==, !=
+$a = 5;
+$b = 10;
+
+echo "perbanding antara $a dan $b <br>";
+echo "<br> $a < $b = ";
+var_dump($a < $b);
+echo "<br> $a > $b = ";
+var_dump($a > $b);
+echo "<br> $a <= $b = ";
+var_dump($a <= $b);
+echo "<br> $a >= $b = ";
+var_dump($a >= $b);
+echo "<br> $a == $b = ";
+var_dump($a == $b);
+echo "<br> $a != $b = ";
+var_dump($a != $b);
+
+?>
