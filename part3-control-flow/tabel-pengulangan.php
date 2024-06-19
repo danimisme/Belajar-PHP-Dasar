f90c3804c06a01d2f981d43a1f380833bf2365d7
danimisme <msubhanr53@gmail.com>
2024-06-19 23:55:19 +0700

    menggunakan gaya templating

--- part3-control-flow/tabel-pengulangan.php
+++ part3-control-flow/tabel-pengulangan.php
@@ -6,7 +6,7 @@
     <title>Tabel Pengulangan</title>
 </head>
 <body>
-    <table border="1" cellpadding="10" cellspacing="0">
+    <!-- <table border="1" cellpadding="10" cellspacing="0">
         <?php
         for($i=1;$i <= 3; $i++){
             echo "<tr>";
@@ -15,8 +15,18 @@
             }
             echo "</tr>";
         }
-
         ?>
+    </table> -->
+
+    <!-- Menggunakan gaya templating -->
+    <table border="1" cellpadding="10" cellspacing="0">
+        <?php for($i=1; $i<=3; $i++) { ?>
+            <tr border="1" cellpadding="10" cellspacing="0" >
+                <?php for($j=1; $j<=5; $j++){?>
+                    <td> <?php echo"$i,$j" ?></td>
+                <?php }?>
+            </tr>
+        <?php } ?>
     </table>
 </body>
 </html>
