f2d0c91ec9098599d72e00ade99801ba0a24878b
danimisme <msubhanr53@gmail.com>
2024-06-20 16:56:32 +0700

    membuat daftar mahasiswa

---
+++ part7-get-post/1-daftar-mahasiswa.php
@@ -0,0 +1,42 @@
+<?php
+
+$mahasiswa = [[
+    "nama" => "Muhammad Subhan",
+    "npm" => "201943500087",
+    "email" => "msubhanr53@gmail.com",
+    "jurusan" => "Teknik Informatika",
+    "gambar" => "../part6-associative-array/img/mark.jpg"
+],
+[
+    "nama" => "Sandhika Galih",
+    "npm" => "201943500001",
+    "email" => "sandhika@gmail.com",
+    "jurusan" => "Teknik Informatika",
+    "gambar" => "../part6-associative-array/img/elon.jpg"
+]  
+
+];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan get dan post</title>
+</head>
+<body>
+    <h1>Daftar Mahasiswa</h1>
+    <ul>
+    <?php foreach($mahasiswa as $mhs) : ?>
+        <li>
+            <a href="2-detail-mahasiswa.php"><?= $mhs["nama"]; ?></a>
+        </li>
+    <?php endforeach; ?>
+
+    </ul>
+
+
+</body>
+</html>
--- part7-get-post/1-latihan.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-//$_GET
-$_GET["nama"] = "Muhammad Subhan";
-$_GET["nrp"] = "201943500087";
-var_dump($_GET);
-
-?>
