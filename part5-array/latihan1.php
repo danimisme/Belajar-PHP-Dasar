35df1d4e52a7fda4752d6f0f6fb797efe292225a
danimisme <msubhanr53@gmail.com>
2024-06-20 14:05:23 +0700

    membuat array dengan cara lama

---
+++ part5-array/latihan1.php
@@ -0,0 +1,13 @@
+<?php
+//Array
+//variabel yang dapat memiliki banyak nilai
+//elemen pada array boleh memiliki tipe data ynag berbeda
+
+//membuat array
+//cara lama
+$hari = array("Senin","Selasa","Rabu","Kamis","Jum'at","Sabtu","Minggu");
+
+
+
+
+?>
