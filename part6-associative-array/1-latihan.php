5290a13978e37467d8cd769b13bf4b7fb3755205
danimisme <msubhanr53@gmail.com>
2024-06-20 15:31:58 +0700

    membuat array associative multi dimensi

--- part6-associative-array/1-latihan.php
+++ part6-associative-array/1-latihan.php
@@ -2,13 +2,21 @@
 // array associative 
 // keynya adalah string yang kita biat sendiri
 
-$mahasiswa = [
-    "nama" => "Muhammad Subhan",
-    "npm" => "201943500087",
-    "email" => "msubhanr53@gmail.com",
-    "jurusan" => "Teknik Informatika"
+$mahasiswa = [[
+        "nama" => "Muhammad Subhan",
+        "npm" => "201943500087",
+        "email" => "msubhanr53@gmail.com",
+        "jurusan" => "Teknik Informatika"
+    ],
+    [
+        "nama" => "Sandhika Galih",
+        "npm" => "201943500001",
+        "email" => "sandhika@gmail.com",
+        "jurusan" => "Teknik Informatika"
+    ]  
+
 ];
 
-echo $mahasiswa["nama"];
+echo $mahasiswa[1]["nama"];
 
 ?>
