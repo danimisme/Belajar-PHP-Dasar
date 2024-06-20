eb4c1c6778b4cb6a42a96daab1a05f2a5d613473
danimisme <msubhanr53@gmail.com>
2024-06-20 12:41:29 +0700

    membuat fungsi mktime

---
+++ part4-function/3-mktime.php
@@ -0,0 +1,10 @@
+<?php 
+//mktime
+//membuat detik sendiri
+//mktime(jam,menit,detik,bulan,tanggal,tahun)
+
+//encari hari pada tanggal 24 desember 1998
+echo date("l", mktime(0,0,0,12,24,1998));
+
+
+?>
