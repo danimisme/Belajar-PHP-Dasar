4be83be2ae4969cc6cf039b574a877e691413634
danimisme <msubhanr53@gmail.com>
2024-06-20 12:21:37 +0700

    fungsi date

---
+++ part4-function/1-date.php
@@ -0,0 +1,15 @@
+<?php
+    // menampilkan hari sata
+    echo date("l");
+    echo "<br>";
+
+    //menampilkan bulan 
+    echo "<br>";
+    echo date("m");
+    echo "<br>";
+    echo date("M");
+
+    //menampillkan tanggal lengkap
+    echo "<br>";
+    echo date("l, d-M-Y")
+?>
