a1cd65b6cf7e81cb90026a18ebb2c918553015d3
danimisme <msubhanr53@gmail.com>
2024-06-20 15:26:11 +0700

    membuat associative array

---
+++ part6-associative-array/1-latihan.php
@@ -0,0 +1,14 @@
+<?php
+// array associative 
+// keynya adalah string yang kita biat sendiri
+
+$mahasiswa = [
+    "nama" => "Muhammad Subhan",
+    "npm" => "201943500087",
+    "email" => "msubhanr53@gmail.com",
+    "jurusan" => "Teknik Informatika"
+];
+
+echo $mahasiswa["nama"];
+
+?>
