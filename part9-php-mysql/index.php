37af0cc930b4d2ce2e87634fac285cfbbbad286c
danimisme <msubhanr53@gmail.com>
2024-06-21 19:32:30 +0700

    membuat tabel sederhana

---
+++ part9-php-mysql/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Admin</title>
+</head>
+<body>
+    <h1>Daftar Mahasiswa</h1>
+    <table border="1" cellpadding="10" cellspacing="0" >
+    <tr>
+        <th>No.</th>
+        <th>Aksi</th>
+        <th>Gambar</th>
+        <th>NRP</th>
+        <th>Nama</th>
+        <th>Email</th>
+        <th>Jurusan</th>
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>
+            <a href="">ubah</a>
+            | <a href="">hapus</a>
+        </td>
+        <td>
+            <img src="img/dhani.jpg" alt="" width="50">
+        </td>
+        <td>043040023</td>
+        <td>Muhammad Subhan</td>
+        <td>msubhanr53@gmail.com</td>
+        <td>Teknik Informatika</td>
+    </tr>
+
+
+    </table>
+</body>
+</html>
