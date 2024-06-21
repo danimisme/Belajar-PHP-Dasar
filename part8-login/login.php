8be51aca50a306f3b5453b2f8c3a2ee541ecc786
danimisme <msubhanr53@gmail.com>
2024-06-21 14:32:43 +0700

    menambahkan logic sederhana untuk login

--- part8-login/login.php
+++ part8-login/login.php
@@ -1,3 +1,19 @@
+<?php
+// cek apakah tombol submit sudah di tekan apa belum
+if (isset($_POST["submit"])) {
+    //cek username & password
+    if ( $_POST["username"] == "admin" && $_POST["password"] == "123") {
+        //jika benar redirect ke halaman admin
+        header("Location: admin.php");
+        exit;
+    } else {
+        //jika salah
+        $error = true;
+    }
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +23,11 @@
 </head>
 <body>
     <h1>Login Admin</h1>
+
+    <?php if(isset($error)) : ?>
+    <p style="color: red; font-style:italic;" >Username atau password salah</p>
+    <?php endif; ?>
+
     <form action="" method="post">
     <div>
         <label for="username">Username :</label>
