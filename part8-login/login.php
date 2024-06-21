0c2577c150484549e5fae22d1536150401d80c5c
danimisme <msubhanr53@gmail.com>
2024-06-21 14:25:08 +0700

    membuat halaman login sederhana

---
+++ part8-login/login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <h1>Login Admin</h1>
+    <form action="" method="post">
+    <div>
+        <label for="username">Username :</label>
+        <input id="username" type="text" name="username"> 
+    </div>
+    <div>
+        <label for="password">Password :</label>
+        <input id="password" type="password" name="password">
+    </div>
+    <div>
+        <button type="submit" name="submit">Login</button>
+    </div>
+
+    </form>
+</body>
+</html>
