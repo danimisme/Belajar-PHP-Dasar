542056858d8673fd234de72fc258add4d6fc81a0
danimisme <msubhanr53@gmail.com>
2024-06-21 20:09:34 +0700

    menghubungkan tabel ke database mysql

--- part9-php-mysql/index.php
+++ part9-php-mysql/index.php
@@ -1,3 +1,38 @@
+<?php
+//koneksi ke database
+$conn = mysqli_connect("localhost", "root", "","phpdasar");
+//ambil data dari tabel mahasiswa / query data mahasiswa
+$result = mysqli_query($conn, "SELECT * FROM mahasiswa");
+
+if ( !$result) {
+    echo mysqli_error;
+}
+
+// ambil data (fetch) mahasiswa dari object result
+// mysqli_fetch_row() // mengembalikan array numerik
+// $mhs = mysqli_fetch_row($result);
+// var_dump($mhs);
+
+// myslqi_fetch_assoc() // mengembailan array associative
+// $mhs = mysqli_fetch_assoc($result);
+// var_dump($mhs);
+
+// mysqli_fetch_array() // mengembalikan array numerik & associative
+// $mhs = mysqli_fetch_array($result);
+// var_dump($mhs);
+
+// mysqli_fetch_object() // mengembailkan object
+// $mhs = mysqli_fetch_object($result);
+// var_dump($mhs);
+
+// menggunakan mysqli_fetch_assoc
+
+// while ( $mhs = mysqli_fetch_assoc($result) ){
+//     var_dump($mhs);
+// };
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -17,21 +52,25 @@
         <th>Email</th>
         <th>Jurusan</th>
     </tr>
+    <?php $i=1; ?>
+    <?php while($row = mysqli_fetch_assoc($result)) : ?>
+
     <tr>
-        <td>1</td>
+        <td> <?= $i ?></td>
         <td>
             <a href="">ubah</a>
             | <a href="">hapus</a>
         </td>
         <td>
-            <img src="img/dhani.jpg" alt="" width="50">
+            <img src="img/<?= $row["gambar"] ?>" alt="" width="50">
         </td>
-        <td>043040023</td>
-        <td>Muhammad Subhan</td>
-        <td>msubhanr53@gmail.com</td>
-        <td>Teknik Informatika</td>
+        <td><?= $row["nrp"] ?></td>
+        <td><?= $row["nama"] ?></td>
+        <td><?= $row["email"] ?></td>
+        <td><?= $row["jurusan"] ?></td>
     </tr>
-
+    <?php $i++; ?>
+    <?php endwhile; ?>
 
     </table>
 </body>
