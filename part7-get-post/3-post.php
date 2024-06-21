475bbf0106d435c66c5989036abefee84db476c0
danimisme <msubhanr53@gmail.com>
2024-06-21 14:04:43 +0700

    membuat form dengan method post

---
+++ part7-get-post/3-post.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Post</title>
+</head>
+<body>
+    <form action="4-latihan.php" method="post">
+        <label for="nama">Masukan Nama</label>
+        <input type="text" name="nama">
+        <br>
+        <button type="submit" name="submit">Kirim</button>
+    </form>
+</body>
+</html>
