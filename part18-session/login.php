9d5db87a95191d5c23829d863ac168355f66e2f0
danimisme <msubhanr53@gmail.com>
2024-06-23 15:06:28 +0700

    menambahkan session

--- part18-session/login.php
+++ part18-session/login.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 require 'functions.php';
 
 if( isset($_POST["login"])) {
@@ -12,6 +13,8 @@ if( isset($_POST["login"])) {
         // cek password
         $row = mysqli_fetch_assoc($result);
         if (password_verify($password, $row["password"])){
+            //set session
+            $_SESSION["login"] = true;
             header("Location: index.php ");
             exit;
         }
