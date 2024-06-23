fc7c0bdb46ce73217fcc10aa062d6cfc3cee3569
danimisme <msubhanr53@gmail.com>
2024-06-23 16:18:29 +0700

    menambahkan cookie ketika login

--- part19-cookie/login.php
+++ part19-cookie/login.php
@@ -1,5 +1,18 @@
 <?php
 session_start();
+require 'functions.php';
+if ($_COOKIE['id'] && isset($_COOKIE['key'])) {
+    $id = $_COOKIE['id'];
+    $key = $_COOKIE['key'];
+
+    $result = mysqli_query($conn, "SELECT username from user WHERE id = $id");
+    $row = mysqli_fetch_assoc($result);
+    
+    if ($key === hash('sha256', $row['username'])){
+        $_SESSION['login'] = true;
+    }
+
+}
 
 if (isset($_SESSION["login"])) {
     header("Location: index.php");
@@ -7,7 +20,7 @@ if (isset($_SESSION["login"])) {
 }
 
 
-require 'functions.php';
+
 
 if( isset($_POST["login"])) {
     $username = $_POST["username"];
@@ -22,6 +35,13 @@ if( isset($_POST["login"])) {
         if (password_verify($password, $row["password"])){
             //set session
             $_SESSION["login"] = true;
+            //cek remember me
+            if (isset($_POST['remember'])) {
+                //buat cookie
+                setcookie('id', $row['id'], time()+60);
+                setcookie('key', hash('sha256', $row['username']), time()+60);
+            }
+
             header("Location: index.php ");
             exit;
         }
@@ -56,6 +76,10 @@ if( isset($_POST["login"])) {
             <li>
                 <label for="password">Password</label>
                 <input type="password" name="password" id="password">
+            </li>
+            <li>
+                <input type="checkbox" name="remember" id="remember">
+                <label for="remember">Remember me</label>
             </li>
             <button type="submit" name="login">Login</button>
         </ul>
