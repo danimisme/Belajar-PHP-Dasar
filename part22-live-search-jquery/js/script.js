58cbc98ca0987a7ad0f05d5ee98800ae33435af1
danimisme <msubhanr53@gmail.com>
2024-06-23 22:58:59 +0700

    menggunakan jquery untuk fungsi search

--- part22-live-search-jquery/js/script.js
+++ part22-live-search-jquery/js/script.js
@@ -1,20 +1,6 @@
-//  ambil element yang dibutuhkan
-var keyword = document.getElementById("keyword");
-var tombolCari = document.getElementById("tombol-cari");
-var container = document.getElementById("container");
-
-// tambahkan event ketika keyword search diketik
-keyword.addEventListener("keyup", function () {
-  //buat object ajax
-  var xhr = new XMLHttpRequest();
-
-  // cek kesiapan ajax
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-      container.innerHTML = xhr.responseText;
-    }
-  };
-
-  xhr.open("GET", "ajax/mahasiswa.php?keyword=" + keyword.value, true);
-  xhr.send();
+$(document).ready(function () {
+  //event ketika keyword seach diketik
+  $("#keyword").on("keyup", function () {
+    $("#container").load("ajax/mahasiswa.php?keyword=" + $("#keyword").val());
+  });
 });
